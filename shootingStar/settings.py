54928026fb5b02f0e41e2fa93eaa719660044aef
frannajaya <franna.jaya@gmail.com>
2021-05-18 22:21:17 +0700

    [CHANGE] fix settings and add new gitignore

--- shootingStar/settings.py
+++ shootingStar/settings.py
@@ -14,9 +14,6 @@ from pathlib import Path
 import django_heroku
 import os
 
-# Activate Django-Heroku.
-django_heroku.settings(locals())
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -131,3 +128,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
